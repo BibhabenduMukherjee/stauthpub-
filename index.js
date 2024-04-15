187a0d8aa3268a686968cb0d954a050e4cc117dd
= <mukherjee4004@gmail.com>
2024-04-15 18:32:15 +0530

    final add #3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require("cors")
+const nodemailer = require('nodemailer')
 const app = express();
 const datas = require('./data')
 const timeserise = require('./timeserise')
@@ -64,9 +65,48 @@ app.post("/getstocktimeserise" , (req,res)=>{
   }
   res.status(200).json({info : t , tabledata })
 })
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "stocktradeappbcet@gmail.com",
+    pass: process.env.PASS,
+  },
+});
+var output =
+`
+    
+    <p>Successfully  Trained !! Thanks for your first model training </p>
+   
+    
+    <p><strong>This is an automatically generated mail. Please do not reply back.</strong></p>
+    
+    <p>Regards,</p>
+    <p>StockTrade</p>
+`;
+
+app.post("/sendemail", (req,res) => {
+  const {email} = req.body;
+  var mailOptions = {
+    from: "stocktradeappbcet@gmail.com",
+    to: email,
+    subject: "Email Verification", // Subject line
+    html: output, // plain text body
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(info);
+  });
+  res.send("ok")
+
+})
 
 let PORT = process.env.PORT || 3002
 app.listen(PORT,()=>{
      
     console.log(`Server Listening.. On Port ${PORT}`);
 })
+
+// https://stockpriceinterface.vercel.app/dashboard/mymodel?modelname=Ad_Netflix_NFLX_bm_661b4944333a8b3a19032b90_mukherjeeb_5&userid=bm_661b4944333a8b3a19032b90
